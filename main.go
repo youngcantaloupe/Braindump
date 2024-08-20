125132358dadd38ccd126d9310a33084a4fef793
youngcantaloupe <portersampson@gmail.com>
2024-08-19 19:34:24 -0600

    Added: Basic user input
    KeyMsg checks if msg is a keypress. If it's enter: capture state of
    textInput (state of m stored as string), print that value, return model
    and no cmd. Loops until keypress is esc or ctrlc cmd returns Quit.

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func main() { 
+    p := tea.NewProgram(initialModel())
+    if _, err := p.Run(); err != nil {
+        log.Fatal(err)
+    }
+}
+
+type (
+	errMsg error
+)
+
+type model struct {
+	textInput textinput.Model
+	err       error
+}
+
+func initialModel() model {
+	ti := textinput.New()
+	ti.Placeholder = "Sample text"
+	ti.Focus()
+	ti.CharLimit = 156
+	ti.Width = 20
+
+	return model{
+		textInput: ti,
+		err:       nil,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+    return textinput.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+        case tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+        case tea.KeyEnter:
+            fmt.Println(m.textInput.Value())
+            return m, nil
+		}
+
+	case errMsg:
+		m.err = msg
+		return m, nil
+	}
+
+	m.textInput, cmd = m.textInput.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return fmt.Sprintf(
+        "List items:\n\n%s\n\n%s",
+		m.textInput.View(),
+		"(esc to quit)",
+	) + "\n"
+}
